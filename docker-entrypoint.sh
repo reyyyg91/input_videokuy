5715341ee9525713719d8d415fe1f49b79894e67
Muhammad Ilham <ilhamthetamvan@gmail.com>
2026-05-11 09:42:10 +0800

    bug fix v3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Railway biasanya menyediakan PORT. Kalau tidak ada, default ke 80.
+export PORT="${PORT:-80}"
+
 # Ganti ${PORT} dengan nilai environment variable
 envsubst '\${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
